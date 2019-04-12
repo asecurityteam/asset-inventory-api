5346daf1f750d7f5ad943d6bb8eb78c484d42438
Mike Rheinheimer <mrheinheimer@ohmyhead.com>
2019-04-12 11:18:04 -0500

    format it

--- pkg/logs/db_error.go
+++ pkg/logs/db_error.go
@@ -5,6 +5,7 @@ type DBOpenError struct {
 	Message string `logevent:"message,default=database-open-error"`
 	Reason  string `logevent:"reason"`
 }
+
 // DBPingError is logged when there is a failure to "Ping" the database, which is required to establish Postgres connection
 type DBPingError struct {
 	Message string `logevent:"message,default=database-ping-error"`
