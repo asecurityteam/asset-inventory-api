8417d6f5ffe7de5a30695c33b70e2c7f1c8fce3a
Zachary Lozano <zlozano@utexas.edu>
2019-05-24 11:45:03 -0500

    add get handler for partitions (#20)
    * add get handler for partitions
    
    * fix handle registration

--- pkg/domain/storage.go
+++ pkg/domain/storage.go
@@ -12,6 +12,11 @@ type PartitionGenerator interface {
 	GeneratePartitionWithTimestamp(context.Context, time.Time) error
 }
 
+// PartitionsGetter is used to fetch a list of partitions
+type PartitionsGetter interface {
+	GetPartitions(context.Context) ([]Partition, error)
+}
+
 // CloudAssetStorer interface provides functions for inserting cloud assets
 type CloudAssetStorer interface {
 	Store(context.Context, CloudAssetChanges) error
@@ -27,11 +32,19 @@ type CloudAssetByHostnameFetcher interface {
 	FetchByHostname(ctx context.Context, when time.Time, hostname string) ([]CloudAssetDetails, error)
 }
 
+// Partition represents a database partition with the specified time range
+type Partition struct {
+	Name      string
+	CreatedAt time.Time
+	Begin     time.Time
+	End       time.Time
+}
+
 // PartitionConflict is used to indicate a partition exists which overlaps with a partition requested to be created
 type PartitionConflict struct {
 	Name string
 }
 
 func (e PartitionConflict) Error() string {
-	return fmt.Sprintf("A partition already exists which overlaps witht the requested partition, %s", e.Name)
+	return fmt.Sprintf("A partition already exists which overlaps with the requested partition, %s", e.Name)
 }
