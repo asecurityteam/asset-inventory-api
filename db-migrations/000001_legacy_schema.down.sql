ef7a1e7c33c2be7cace104feecf5f3b33868e731
Grygoriy Ensary <gensary@atlassian.com>
2020-01-30 10:40:48 -0600

    only drop specific tables

--- db-migrations/000001_legacy_schema.down.sql
+++ db-migrations/000001_legacy_schema.down.sql
@@ -1,8 +1,7 @@
 BEGIN;
--- NB, this probably should not be done or prevented from being run in production
--- from the higher-voted answer here:  https://stackoverflow.com/questions/3327312/how-can-i-drop-all-the-tables-in-a-postgresql-database
-DROP SCHEMA public CASCADE;
-CREATE SCHEMA
-    public;
-GRANT ALL ON SCHEMA public TO public;
+DROP TABLE IF EXISTS aws_events_ips_hostnames;
+DROP TABLE IF EXISTS aws_resources;
+DROP TABLE IF EXISTS aws_ips;
+DROP TABLE IF EXISTS aws_hostnames;
+DROP TABLE IF EXISTS partitions;
 COMMIT;
