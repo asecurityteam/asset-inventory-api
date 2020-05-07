c24043aa53fc9ba039ce145c13c4875e9cf96c43
gabriel case <gcase@atlassian.com>
2020-05-07 10:13:16 -0500

    adding new table logic

--- db-migrations/000005_adding_owners.up.sql
+++ db-migrations/000005_adding_owners.up.sql
@@ -1,3 +1,12 @@
 -- schema changes for account owners and champions
 BEGIN;
+create table champions(
+    id serial primary key,
+    champion varchar not null,
+)
+
+alter table aws_account
+-- accounts may or may not have champions associated with them, so we may not need to enforce a foreign key restraint
+add column champion_id null unique
+
 COMMIT;
