de064a01f5d2c16d923f0c9596219575c17774c1
Sydney Teh <steh@atlassian.com>
2020-06-11 14:30:37 -0500

    Add unit tests for account owner

--- pkg/handlers/v1/insert_account_owner_test.go
+++ pkg/handlers/v1/insert_account_owner_test.go
@@ -18,7 +18,7 @@ func newInsertAccountOwnerHandler(storer domain.AccountOwnerStorer) *AccountOwne
 	}
 }
 
-func testInsertInput() AccountOwner {
+func testInputWithChampion() AccountOwner {
 	return AccountOwner{
 		AccountID: "awsaccountid123",
 		Owner: Person{
@@ -38,6 +38,18 @@ func testInsertInput() AccountOwner {
 	}
 }
 
+func testInputWithoutChampion() AccountOwner {
+	return AccountOwner{
+		AccountID: "awsaccountid123",
+		Owner: Person{
+			Name:  "john dane",
+			Login: "jdane",
+			Email: "jdane@atlassian.com",
+			Valid: true,
+		},
+	}
+}
+
 func TestInsertAccountOwnerError(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
@@ -45,6 +57,28 @@ func TestInsertAccountOwnerError(t *testing.T) {
 	storage := NewMockAccountOwnerStorer(ctrl)
 	storage.EXPECT().StoreAccountOwner(gomock.Any(), gomock.Any()).Return(errors.New("error"))
 
-	e := newInsertAccountOwnerHandler(storage).Handle(context.Background(), testInsertInput())
+	e := newInsertAccountOwnerHandler(storage).Handle(context.Background(), testInputWithChampion())
 	assert.NotNil(t, e)
+}
+
+func TestInsertAccountOwnerWithChampion(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	storage := NewMockAccountOwnerStorer(ctrl)
+	storage.EXPECT().StoreAccountOwner(gomock.Any(), gomock.Any()).Return(nil)
+
+	e := newInsertAccountOwnerHandler(storage).Handle(context.Background(), testInputWithChampion())
+	assert.Nil(t, e)
+}
+
+func TestInsertAccountOwnerWithoutChampion(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	storage := NewMockAccountOwnerStorer(ctrl)
+	storage.EXPECT().StoreAccountOwner(gomock.Any(), gomock.Any()).Return(nil)
+
+	e := newInsertAccountOwnerHandler(storage).Handle(context.Background(), testInputWithoutChampion())
+	assert.Nil(t, e)
 }
