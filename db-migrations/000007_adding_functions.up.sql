a17e21054f66976f9adabb7881d85b2973bff9cc
Rajiv Soni <rsoni@atlassian.com>
2020-07-24 10:36:13 -0700

    Updated functions to use nested queries

--- db-migrations/000007_adding_functions.up.sql
+++ db-migrations/000007_adding_functions.up.sql
@@ -24,40 +24,51 @@ CREATE OR REPLACE FUNCTION get_resource_by_arn_id(aid VARCHAR, ts TIMESTAMP)
             )
 AS
 $$
-DECLARE
-    var_aws_account_id INTEGER;
 BEGIN
-    CREATE TEMP TABLE IF NOT EXISTS temp_aws_resource_table AS
-    SELECT pria.private_ip,
-           puia.public_ip,
-           puia.aws_hostname,
-           rt.resource_type,
-           aa.account,
-           ar.region,
-           res.meta,
-           res.aws_account_id
-    FROM aws_resource res
-             LEFT JOIN aws_region ar ON res.aws_region_id = ar.id
-             LEFT JOIN aws_account aa ON res.aws_account_id = aa.id
-             LEFT JOIN aws_resource_type rt ON res.aws_resource_type_id = rt.id
-             LEFT JOIN aws_public_ip_assignment puia ON res.id = puia.aws_resource_id
-             LEFT JOIN aws_private_ip_assignment pria ON res.id = pria.aws_resource_id
-    WHERE res.arn_id = aid
-      AND puia.not_before < ts
-      AND (puia.not_after IS NULL OR puia.not_after > ts)
-      AND pria.not_before < ts
-      AND (pria.not_after IS NULL OR pria.not_after > ts);
-
-    SELECT temp_aws_resource_table.aws_account_id
-    INTO var_aws_account_id
-    FROM temp_aws_resource_table FETCH FIRST ROW ONLY;
-
-    RETURN QUERY SELECT a.*, b.*
-                 FROM temp_aws_resource_table AS a
-                          LEFT JOIN (
-                     SELECT *
-                     FROM get_owner_and_champions_by_account_id(var_aws_account_id)
-                 ) AS b ON a.account = b.t_account;
+    RETURN QUERY WITH wres AS (SELECT pria.private_ip,
+                                      puia.public_ip,
+                                      puia.aws_hostname,
+                                      rt.resource_type,
+                                      aa.account,
+                                      ar.region,
+                                      res.meta,
+                                      res.aws_account_id
+                               FROM aws_resource res
+                                        LEFT JOIN aws_region ar ON res.aws_region_id = ar.id
+                                        LEFT JOIN aws_account aa ON res.aws_account_id = aa.id
+                                        LEFT JOIN aws_resource_type rt ON res.aws_resource_type_id = rt.id
+                                        LEFT JOIN aws_public_ip_assignment puia ON res.id = puia.aws_resource_id
+                                        LEFT JOIN aws_private_ip_assignment pria ON res.id = pria.aws_resource_id
+                               WHERE res.arn_id = aid
+                                 AND puia.not_before < ts
+                                 AND (puia.not_after IS NULL OR puia.not_after > ts)
+                                 AND pria.not_before < ts
+                                 AND (pria.not_after IS NULL OR pria.not_after > ts))
+                 SELECT wres.private_ip,
+                        wres.public_ip,
+                        wres.aws_hostname,
+                        wres.resource_type,
+                        wres.account,
+                        wres.region,
+                        wres.meta,
+                        wres.aws_account_id,
+                        b.t_account,
+                        b.t_login,
+                        b.t_email,
+                        b.t_name,
+                        b.t_valid,
+                        b.p_login,
+                        b.p_email,
+                        b.p_name,
+                        b.p_valid
+                 FROM wres
+                          JOIN
+                      (
+                          SELECT distinct iwres.aws_account_id, f.*
+                          FROM wres iwres,
+                               LATERAL get_owner_and_champions_by_account_id(iwres.aws_account_id) f
+                      ) b
+                      ON wres.account = b.t_account;
 END;
 $$
     LANGUAGE 'plpgsql';
@@ -78,19 +89,6 @@ CREATE OR REPLACE FUNCTION get_owner_and_champions_by_account_id(id INTEGER)
 AS
 $$
 BEGIN
-    CREATE TEMP TABLE IF NOT EXISTS temp_account_owner_table AS
-    SELECT aa.account,
-           ow.login,
-           ow.email,
-           ow.name,
-           ow.valid,
-           ac.person_id
-    FROM account_owner ao
-             LEFT JOIN aws_account aa ON ao.aws_account_id = aa.id
-             LEFT JOIN person ow ON ao.person_id = ow.id
-             LEFT JOIN account_champion ac ON ao.aws_account_id = ac.aws_account_id
-    WHERE ao.aws_account_id = get_owner_and_champions_by_account_id.id;
-
     RETURN QUERY SELECT t.account,
                         t.login,
                         t.email,
@@ -100,7 +98,17 @@ BEGIN
                         p.email,
                         p.name,
                         p.valid
-                 FROM temp_account_owner_table t
+                 FROM (SELECT aa.account,
+                              ow.login,
+                              ow.email,
+                              ow.name,
+                              ow.valid,
+                              ac.person_id
+                       FROM account_owner ao
+                                LEFT JOIN aws_account aa ON ao.aws_account_id = aa.id
+                                LEFT JOIN person ow ON ao.person_id = ow.id
+                                LEFT JOIN account_champion ac ON ao.aws_account_id = ac.aws_account_id
+                       WHERE ao.aws_account_id = get_owner_and_champions_by_account_id.id) t
                           LEFT JOIN person p ON t.person_id = p.id;
 END;
 $$
@@ -129,35 +137,48 @@ CREATE OR REPLACE FUNCTION get_resource_by_hostname(name VARCHAR, ts TIMESTAMP)
             )
 AS
 $$
-DECLARE
-    var_account_id INTEGER;
 BEGIN
-    CREATE TEMP TABLE IF NOT EXISTS temp_hostname_table AS
-    SELECT ia.public_ip,
-           ia.aws_hostname,
-           res.arn_id,
-           res.meta,
-           ar.region,
-           rt.resource_type,
-           aa.account,
-           aa.id
-    FROM aws_public_ip_assignment ia
-             LEFT JOIN aws_resource res ON ia.aws_resource_id = res.id
-             LEFT JOIN aws_region ar ON res.aws_region_id = ar.id
-             LEFT JOIN aws_resource_type rt ON res.aws_resource_type_id = rt.id
-             LEFT JOIN aws_account aa ON res.aws_account_id = aa.id
-    WHERE ia.aws_hostname = name
-      AND ia.not_before < ts
-      AND (ia.not_after IS NULL OR ia.not_after > ts);
-
-    SELECT temp_hostname_table.id INTO var_account_id FROM temp_hostname_table FETCH FIRST ROW ONLY;
-
-    RETURN QUERY SELECT a.*, b.*
-                 FROM temp_hostname_table AS a
-                          LEFT JOIN (
-                     SELECT *
-                     FROM get_owner_and_champions_by_account_id(var_account_id)
-                 ) AS b ON a.account = b.t_account;
+    RETURN QUERY WITH wres AS (SELECT ia.public_ip,
+                                      ia.aws_hostname,
+                                      res.arn_id,
+                                      res.meta,
+                                      ar.region,
+                                      rt.resource_type,
+                                      aa.account,
+                                      aa.id
+                               FROM aws_public_ip_assignment ia
+                                        LEFT JOIN aws_resource res ON ia.aws_resource_id = res.id
+                                        LEFT JOIN aws_region ar ON res.aws_region_id = ar.id
+                                        LEFT JOIN aws_resource_type rt ON res.aws_resource_type_id = rt.id
+                                        LEFT JOIN aws_account aa ON res.aws_account_id = aa.id
+                               WHERE ia.aws_hostname = name
+                                 AND ia.not_before < ts
+                                 AND (ia.not_after IS NULL OR ia.not_after > ts))
+                 SELECT wres.public_ip,
+                        wres.aws_hostname,
+                        wres.arn_id,
+                        wres.meta,
+                        wres.region,
+                        wres.resource_type,
+                        wres.account,
+                        wres.id,
+                        b.t_account,
+                        b.t_login,
+                        b.t_email,
+                        b.t_name,
+                        b.t_valid,
+                        b.p_login,
+                        b.p_email,
+                        b.p_name,
+                        b.p_valid
+                 FROM wres
+                          JOIN
+                      (
+                          SELECT distinct iwres.id, f.*
+                          FROM wres iwres,
+                               LATERAL get_owner_and_champions_by_account_id(iwres.id) f
+                      ) b
+                      ON wres.account = b.t_account;
 END;
 $$
     LANGUAGE 'plpgsql';
@@ -184,34 +205,46 @@ CREATE OR REPLACE FUNCTION get_resource_by_private_ip(pip INET, ts TIMESTAMP)
             )
 AS
 $$
-DECLARE
-    var_account_id INTEGER;
 BEGIN
-    CREATE TEMP TABLE IF NOT EXISTS temp_private_ip_table AS
-    SELECT ia.private_ip,
-           res.arn_id,
-           res.meta,
-           ar.region,
-           rt.resource_type,
-           aa.account,
-           aa.id
-    FROM aws_private_ip_assignment ia
-             LEFT JOIN aws_resource res ON ia.aws_resource_id = res.id
-             LEFT JOIN aws_region ar ON res.aws_region_id = ar.id
-             LEFT JOIN aws_resource_type rt ON res.aws_resource_type_id = rt.id
-             LEFT JOIN aws_account aa ON res.aws_account_id = aa.id
-    WHERE ia.private_ip = pip
-      AND ia.not_before < ts
-      AND (ia.not_after IS NULL OR ia.not_after > ts);
-
-    SELECT temp_private_ip_table.id INTO var_account_id FROM temp_private_ip_table FETCH FIRST ROW ONLY;
-
-    RETURN QUERY SELECT a.*, b.*
-                 FROM temp_private_ip_table AS a
-                          LEFT JOIN (
-                     SELECT *
-                     FROM get_owner_and_champions_by_account_id(var_account_id)
-                 ) AS b ON a.account = b.t_account;
+    RETURN QUERY WITH wres AS (SELECT ia.private_ip,
+                                      res.arn_id,
+                                      res.meta,
+                                      ar.region,
+                                      rt.resource_type,
+                                      aa.account,
+                                      aa.id
+                               FROM aws_private_ip_assignment ia
+                                        LEFT JOIN aws_resource res ON ia.aws_resource_id = res.id
+                                        LEFT JOIN aws_region ar ON res.aws_region_id = ar.id
+                                        LEFT JOIN aws_resource_type rt ON res.aws_resource_type_id = rt.id
+                                        LEFT JOIN aws_account aa ON res.aws_account_id = aa.id
+                               WHERE ia.private_ip = pip
+                                 AND ia.not_before < ts
+                                 AND (ia.not_after IS NULL OR ia.not_after > ts))
+                 SELECT wres.private_ip,
+                        wres.arn_id,
+                        wres.meta,
+                        wres.region,
+                        wres.resource_type,
+                        wres.account,
+                        wres.id,
+                        b.t_account,
+                        b.t_login,
+                        b.t_email,
+                        b.t_name,
+                        b.t_valid,
+                        b.p_login,
+                        b.p_email,
+                        b.p_name,
+                        b.p_valid
+                 FROM wres
+                          JOIN
+                      (
+                          SELECT distinct iwres.id, f.*
+                          FROM wres iwres,
+                               LATERAL get_owner_and_champions_by_account_id(iwres.id) f
+                      ) b
+                      ON wres.account = b.t_account;
 END;
 $$
     LANGUAGE 'plpgsql';
@@ -239,35 +272,48 @@ CREATE OR REPLACE FUNCTION get_resource_by_public_ip(pip INET, ts TIMESTAMP)
             )
 AS
 $$
-DECLARE
-    var_account_id INTEGER;
 BEGIN
-    CREATE TEMP TABLE IF NOT EXISTS temp_public_ip_table AS
-    SELECT ia.public_ip,
-           ia.aws_hostname,
-           res.arn_id,
-           res.meta,
-           ar.region,
-           rt.resource_type,
-           aa.account,
-           aa.id
-    FROM aws_public_ip_assignment ia
-             LEFT JOIN aws_resource res ON ia.aws_resource_id = res.id
-             LEFT JOIN aws_region ar ON res.aws_region_id = ar.id
-             LEFT JOIN aws_resource_type rt ON res.aws_resource_type_id = rt.id
-             LEFT JOIN aws_account aa ON res.aws_account_id = aa.id
-    WHERE ia.public_ip = pip
-      AND ia.not_before < ts
-      AND (ia.not_after IS NULL OR ia.not_after > ts);
-
-    SELECT temp_public_ip_table.id INTO var_account_id FROM temp_public_ip_table FETCH FIRST ROW ONLY;
-
-    RETURN QUERY SELECT a.*, b.*
-                 FROM temp_public_ip_table AS a
-                          LEFT JOIN (
-                     SELECT *
-                     FROM get_owner_and_champions_by_account_id(var_account_id)
-                 ) AS b ON a.account = b.t_account;
+    RETURN QUERY WITH wres AS (SELECT ia.public_ip,
+                                      ia.aws_hostname,
+                                      res.arn_id,
+                                      res.meta,
+                                      ar.region,
+                                      rt.resource_type,
+                                      aa.account,
+                                      aa.id
+                               FROM aws_public_ip_assignment ia
+                                        LEFT JOIN aws_resource res ON ia.aws_resource_id = res.id
+                                        LEFT JOIN aws_region ar ON res.aws_region_id = ar.id
+                                        LEFT JOIN aws_resource_type rt ON res.aws_resource_type_id = rt.id
+                                        LEFT JOIN aws_account aa ON res.aws_account_id = aa.id
+                               WHERE ia.public_ip = pip
+                                 AND ia.not_before < ts
+                                 AND (ia.not_after IS NULL OR ia.not_after > ts))
+                 SELECT wres.public_ip,
+                        wres.aws_hostname,
+                        wres.arn_id,
+                        wres.meta,
+                        wres.region,
+                        wres.resource_type,
+                        wres.account,
+                        wres.id,
+                        b.t_account,
+                        b.t_login,
+                        b.t_email,
+                        b.t_name,
+                        b.t_valid,
+                        b.p_login,
+                        b.p_email,
+                        b.p_name,
+                        b.p_valid
+                 FROM wres
+                          JOIN
+                      (
+                          SELECT distinct iwres.id, f.*
+                          FROM wres iwres,
+                               LATERAL get_owner_and_champions_by_account_id(iwres.id) f
+                      ) b
+                      ON wres.account = b.t_account;
 END;
 $$
     LANGUAGE 'plpgsql';
