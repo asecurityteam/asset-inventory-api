d00f4efbdbef0e6c00fc3b23bb5ad5a4057d26d8
Alena Khalina <akhalina@atlassian.com>
2020-08-20 17:11:25 -0400

    Add helper function for schema migration + example usage

--- tests/client_test.go
+++ tests/client_test.go
@@ -13,43 +13,66 @@ import (
 )
 
 var assetInventoryAPI *openapi.APIClient
+var schemaVersion int32 = 1
+var maxSchema int32 = 10 // TODO: extrapolate this somewhere?
 
 func TestMain(m *testing.M) {
 	config := openapi.NewConfiguration()
 	appURL := os.Getenv("AIA_APP_URL")
 	config.BasePath = appURL
 	assetInventoryAPI = openapi.NewAPIClient(config)
-	ctx := context.Background()
-
-	// Test on latest db schema
-	for {
-		_, _, err := assetInventoryAPI.DefaultApi.OpsPgsqlV1SchemaVersionStepUpGet(ctx)
-
-		// An error hopefully means that he hit the last schema version
-		if err != nil {
-			break
-		}
-	}
-
 	code := m.Run()
 	os.Exit(code)
 }
 
 func TestHealthcheck(t *testing.T) {
-	tt := []struct {
-		Name string
-	}{
-		{
-			Name: "Health Check",
-		},
-	}
-	for _, test := range tt {
-		fn := func(t *testing.T) {
-			ctx := context.Background()
-			resp, err := assetInventoryAPI.DefaultApi.HealthcheckGet(ctx)
-			assert.NoError(t, err, "Health check should produce no errors")
-			assert.Equal(t, resp.StatusCode, http.StatusOK)
+	// Example: check every schema version against health check.
+	for schema := schemaVersion; schema < maxSchema; schema++ {
+		tt := []struct {
+			Name string
+		}{
+			{
+				Name: "Health Check",
+			},
 		}
-		t.Run(test.Name, fn)
+		for _, test := range tt {
+			fn := func(t *testing.T) {
+				ctx := context.Background()
+				resp, err := assetInventoryAPI.DefaultApi.HealthcheckGet(ctx)
+				assert.NoError(t, err, "Health check should produce no errors")
+				assert.Equal(t, resp.StatusCode, http.StatusOK)
+			}
+			t.Run(test.Name, fn)
+		}
+
+		err := setSchemaVersion(schema + 1)
+		assert.NoError(t, err, "The database migration should not return an error")
 	}
+}
+
+// Migrate database to the desired schema
+func setSchemaVersion(version int32) error {
+	ctx := context.Background()
+
+	if schemaVersion == version {
+		return nil
+	} else if schemaVersion < version {
+		schema, _, err := assetInventoryAPI.DefaultApi.OpsPgsqlV1SchemaVersionStepUpGet(ctx)
+
+		if err != nil {
+			return err
+		}
+
+		schemaVersion = schema.Version
+	} else {
+		schema, _, err := assetInventoryAPI.DefaultApi.OpsPgsqlV1SchemaVersionStepDownGet(ctx)
+
+		if err != nil {
+			return err
+		}
+
+		schemaVersion = schema.Version
+	}
+
+	return setSchemaVersion(version)
 }
